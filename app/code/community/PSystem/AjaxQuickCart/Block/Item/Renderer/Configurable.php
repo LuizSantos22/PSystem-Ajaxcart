9438025db3889f27ae1e3583601f4f5e409b73cd
LuizSantos22 <83614706+LuizSantos22@users.noreply.github.com>
2026-04-08 00:58:09 -0300

    Update Configurable.php

--- app/code/community/PSystem/AjaxQuickCart/Block/Item/Renderer/Configurable.php
+++ app/code/community/PSystem/AjaxQuickCart/Block/Item/Renderer/Configurable.php
@@ -30,3 +30,38 @@ class PSystem_AjaxQuickCart_Block_Item_Renderer_Configurable extends Mage_Checko
 		);
 	}
 }
+
+public_html/app/code/community/PSystem/AjaxQuickCart/Block/Item/Renderer.php
+
+<?php
+/**
+ * @category   PSystem
+ * @package    PSystem_AjaxQuickCart
+ * @author     Pascal System <info@pascalsystem.pl>
+ * @version    1.0.2
+ */
+
+/**
+ * Pascal Quick Cart Ajax item layer renderer
+ * 
+ * @category   PSystem
+ * @package    PSystem_AjaxQuickCart
+ * @author     Pascal System <info@pascalsystem.pl>
+ * @version    1.0.2
+ */
+class PSystem_AjaxQuickCart_Block_Item_Renderer extends Mage_Checkout_Block_Cart_Item_Renderer {
+/**
+ * Get item delete url
+ *
+ * @return string
+ */
+	public function getDeleteUrl() {
+		return $this->getUrl(
+			'checkout/cart/delete',
+			array(
+				'id'=>$this->getItem()->getId(),
+				Mage_Core_Controller_Front_Action::PARAM_NAME_URL_ENCODED => $this->helper('core/url')->getEncodedUrl(Mage::getUrl('checkout/cart/index'))
+			)
+		);
+	}
+}
